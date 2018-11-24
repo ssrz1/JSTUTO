848d40029a8f354a0f6aeed1effee0b2d59f58b4
ssrz1 <sajjadraza00@gmail.com>
2018-11-25 02:03:39 +0500

    Game

--- starter/app.js
+++ starter/app.js
@@ -15,12 +15,28 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-//floor basicaly truncate after decimal part and give us single digit.
-dice = Math.floor(Math.random() * 6 + 1);
-//querySelecter are use to select element from web page.
-document.querySelector("#current-" + activePlayer).textContent = dice;
-//when we use css class we us . operator
 document.querySelector(".dice").style.display = "none";
 
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
 //btn is a function and we are using without btn() simply because btn is a callback function and it will not be called by us instead eventListner will call it.
-document.querySelector(".btn-roll").addEventListener("click", btn);
+//document.querySelector(".btn-roll").addEventListener("click", btn);
+
+//annonymus fuction is function which donot have a name and simply it can not be used again tou btn ka yahein kaam lay lo function bana k
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var dice = Math.floor(Math.random() * 6 + 1);
+
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+});
+
+//floor basicaly truncate after decimal part and give us single digit.
+//dice = Math.floor(Math.random() * 6 + 1);
+
+//querySelecter are use to select element from web page.
+//document.querySelector("#current-" + activePlayer).textContent = dice;
+//when we use css class we us . operator
